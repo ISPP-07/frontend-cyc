391c21e5b10aa09d6d0187b113b8ef9ad6bcc5ef
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2024-02-29 15:05:32 +0100

    feat: Families listing screen

---
+++ src/app/families/layout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from '../components/sidebar'
+import Searchbar from '../components/searchbar'
+
+export default function Layout({ children }) {
+	return (
+		<div className="flex h-full flex-col md:flex-row overflow-x-hidden">
+			<Sidebar />
+			<div className="left-80 relative w-full overflow-x-hidden">
+				<div className="-ml-56 min-h-24 w-full fixed bg-white z-10">
+					<Searchbar />
+				</div>
+				<main className="h-screen w-screen max-w-[1600px] p-6 md:p-12">
+					{children}
+				</main>
+			</div>
+		</div>
+	)
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,19 @@ import Searchbar from './components/searchbar'
 import Sidebar from './components/sidebar'
 
 export default function Home() {
-	const [verModal,setVerModal] = useState(false)
+	const [verModal, setVerModal] = useState(false)
 	return (
-		<main className='wallpaper'>
-			<Sidebar/>
-			<Searchbar/>
-			<Modal isVisible={verModal} onClose={()=>setVerModal(false)} />
-			<button className={`bg-cyan-500 rounded-full top-3/4 left-1/4 w-28 relative ${verModal?'blur':''}`} onClick={()=>setVerModal(true)}> Probar modal</button>
+		<main>
+			<Sidebar />
+			<Searchbar />
+			<Modal isVisible={verModal} onClose={() => setVerModal(false)} />
+			<button
+				className={`bg-cyan-500 rounded-full top-3/4 left-1/4 w-28 relative ${verModal ? 'blur' : ''}`}
+				onClick={() => setVerModal(true)}
+			>
+				{' '}
+				Probar modal
+			</button>
 		</main>
 	)
 }
