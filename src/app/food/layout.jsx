f42adc4e9ac140339c9eda049d7b960a3915331b
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-02 19:40:20 +0100

    fix: Modal now showing background elements

--- src/app/food/layout.jsx
+++ src/app/food/layout.jsx
@@ -13,12 +13,19 @@ export default function Layout({ children }) {
 	}
 
 	return (
-		<main className="absolute bg-white w-full h-full ">
+		<main className="absolute bg-white w-full h-full">
 			<div className="flex h-full flex-col md:flex-row overflow-x-hidden bg-white">
 				<div className="w-full h-full flex-none md:w-64 fixed z-20">
 					<Sidebar />
 				</div>
 				<div className="left-80 relative w-full overflow-x-hidden">
+					<div className="-ml-56 min-h-24 w-full fixed bg-white z-10">
+						<Searchbar
+							buttonText="Añadir elemento"
+							onClickFunction={onClickFunction}
+						/>
+					</div>
+					<main className="p-6 md:p-12">{children}</main>
 					{isVisible ? (
 						<div
 							className={`fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center ${isVisible ? 'z-30' : 'z-0'} `}
@@ -35,15 +42,7 @@ export default function Layout({ children }) {
 							</div>
 						</div>
 					) : (
-						<>
-							<div className="-ml-56 min-h-24 w-full fixed bg-white z-10">
-								<Searchbar
-									buttonText="Añadir elemento"
-									onClickFunction={onClickFunction}
-								/>
-							</div>
-							<main className="p-6 md:p-12">{children}</main>)
-						</>
+						<></>
 					)}
 				</div>
 			</div>
