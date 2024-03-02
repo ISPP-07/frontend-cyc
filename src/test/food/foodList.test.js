fd82acd5051599778c882202e2d8c013bd806e06
JaviGarc1a <javgaragu1@alum.us.es>
2024-03-02 04:25:32 +0100

    fix: Fix test foodList

--- src/test/food/foodList.test.js
+++ src/test/food/foodList.test.js
@@ -1,32 +1,23 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { fetchDataFoods } from './fetchDataFoods'
+import { render, screen, waitFor } from '@testing-library/react'
+import { fetchDataFoods } from '../../app/food/fetch.jsx'
 import FoodList from '../../app/food/FoodList.jsx'
+import { test, expect, describe, jest } from '@jest/globals'
 
-jest.mock('./fetchDataFoods')
+jest.mock('../../app/food/fetch.jsx')
 
 describe('FoodList', () => {
 	test('renders food cards', async () => {
 		const mockData = [
-			{ id: 1, name: 'Food 1', quantity: 13 },
+			{ id: 1, name: 'name 1', quantity: 24 },
 
-			{ id: 2, name: 'Food 2', quantity: 53 }
+			{ id: 2, name: 'name 2', quantity: 59 }
 		]
 		fetchDataFoods.mockResolvedValue(mockData)
-
-		render(<FoodList />)
-
-		const foodCards = await screen.findAllByRole('link', { name: /food/i })
-		expect(foodCards).toHaveLength(mockData.length)
-	})
-
-	test('renders error message when data fetching fails', async () => {
-		const errorMessage = 'Failed to fetch data'
-		fetchDataFoods.mockRejectedValue(new Error(errorMessage))
-
-		render(<FoodList />)
-
-		const errorMessageElement = await screen.findByText(errorMessage)
-		expect(errorMessageElement).toBeInTheDocument()
+		waitFor(async () => {
+			render(<FoodList />)
+			const foodCards = await screen.findAllByRole('link')
+			expect(foodCards).toHaveLength(mockData.length)
+		})
 	})
 })
