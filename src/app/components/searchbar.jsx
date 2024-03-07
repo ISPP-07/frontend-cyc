68cc164f034ee6094158f9327b78ce29d28f1e07
JaviGarc1a <javgaragu1@alum.us.es>
2024-03-07 17:04:50 +0100

    feat: Searchbar final version

--- src/app/components/searchbar.jsx
+++ src/app/components/searchbar.jsx
@@ -1,41 +1,44 @@
 'use client'
 import Image from 'next/image'
-import Link from 'next/link'
 /* eslint-disable no-unused-vars */
 import React from 'react'
 /* eslint-enable no-unused-vars */
 
 const Searchbar = ({
-	buttonText = 'Dar de alta',
-	onClickFunction = () => null
+	text = 'Dar de alta',
+	handleClick = () => {
+		console.log('Funciona')
+	}
 }) => {
 	return (
-		<div className="absolute top-10 left-1/4 w-4/6 h-10 flex items-center">
-			<Image
-				width={18}
-				height={18}
-				src="/magnifier.svg"
-				className="relative z-10 ml-1"
-			></Image>
-			<input
-				className="absolute rounded-3xl border h-9 font-Varela w-9/12 text-black indent-2.5 pl-6"
-				type="text"
-				placeholder=" Buscar.. "
-			/>
-			<button className="bg-blue-500 ml-auto mr-6 relative rounded-full font-Varela text-xs w-8 h-8	">
-				<Image
-					src="/filter.svg"
-					className="ml-1.5"
-					width={20}
-					height={20}
-				></Image>
-			</button>
-			<button
-				onClick={onClickFunction}
-				className="bg-green-700 text-white relative rounded-full font-Varela text-sm w-2/12 h-8 float-right"
-			>
-				{buttonText}
-			</button>
+		<div className="flex w-full justify-end mt-5">
+			<div className="flex justify-around items-center md:w-full w-3/4 p-3 gap-1">
+				<div className="flex w-10/12 border-solid border-[1px] border-gray-400 rounded-full p-1">
+					<button className="outline-none focus:outline-none pl-1">
+						<Image src="/magnifier.svg" width={18} height={18} />
+					</button>
+					<input
+						type="search"
+						placeholder="Buscar..."
+						className="w-full pl-2 bg-transparent outline-none"
+					/>
+				</div>
+				<button className="bg-blue-500 rounded-full p-2 shadow-lg">
+					<Image src="/filter.svg" width={18} height={18} />
+				</button>
+				<button
+					className="bg-[#75AF73] text-white p-2 md:px-3 rounded-full shadow-lg"
+					onClick={handleClick}
+				>
+					<Image
+						src="/plus.svg"
+						width={18}
+						height={18}
+						className="lg:hidden md:block"
+					/>
+					<span className="hidden lg:block text-sm">{text}</span>
+				</button>
+			</div>
 		</div>
 	)
 }
