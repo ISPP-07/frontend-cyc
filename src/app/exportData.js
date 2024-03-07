3928bdff6ff310786b46d7fccd8393df91e55c3d
Nicolás Herrera Lobo <nicoloboo02@gmail.com>
2024-03-07 18:33:12 +0100

    feat: Add export families #31
    A button has been added to export
    the family data to an excel file.

---
+++ src/app/exportData.js
@@ -0,0 +1,7 @@
+import exportFromJSON from "export-from-json";
+
+function exportData(datos,nombre) {
+    exportFromJSON({data:datos, fileName: nombre, exportType: exportFromJSON.types.xls})
+}
+
+export default exportData;
--- src/app/families/page.jsx
+++ src/app/families/page.jsx
@@ -8,6 +8,8 @@ import Card from '../components/card.jsx'
 import Sidebar from '../components/sidebar.jsx'
 import Searchbar from '../components/searchbar.jsx'
 import { fetchFamilies } from './fetchFamilies.js'
+import exportData from '../exportData.js'
+import Image from 'next/image.js'
 
 export default async function BeneficiariesList({ searchParams }) {
 	const data = await fetchFamilies()
@@ -15,6 +17,7 @@ export default async function BeneficiariesList({ searchParams }) {
 	const showModal = () => {
 		window.location.href = '/families?show=true'
 	}
+	console.log(data[1])
 
 	return (
 		<div className="flex h-full flex-col md:flex-row overflow-x-hidden">
@@ -23,7 +26,17 @@ export default async function BeneficiariesList({ searchParams }) {
 				<div className="-ml-56 min-h-24 w-full fixed bg-white z-10">
 					<Searchbar onClickFunction={showModal} />
 				</div>
-				<main className="h-screen w-screen max-w-[1600px] p-6 md:p-12">
+				<main className="h-screen w-screen max-w-[1600px] p-6 md:p-12 flex flex-col">
+					<div className="h-12 w-12 top-28 absolute" >
+						<button className=" bg-green-400 h-8 w-8 rounded-full shadow-2xl mt-3 mr-2" onClick={()=>exportData(data,'Familias')}>
+							<Image
+								src="/excel.svg"
+								className="ml-2"
+								width={15}
+								height={15}>	
+							</Image>
+						</button>
+					</div>
 					<div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 overflow-y-scroll relative top-28">
 						{data.map(family => (
 							<Link href={`/families/${family.id}`} key={family.id}>
