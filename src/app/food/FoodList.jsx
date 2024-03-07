b936dbcac8cf2bced5774c86b1626af9d5fa5a4b
Nicolás Herrera Lobo <nicoloboo02@gmail.com>
2024-03-07 18:42:28 +0100

    feat:Export food  #31
    A button has been added to export the
    food data to an excel file.

--- src/app/food/FoodList.jsx
+++ src/app/food/FoodList.jsx
@@ -1,17 +1,33 @@
+'use client'
 import { fetchDataFoods } from './fetch.jsx'
 import Link from 'next/link.js'
 import FoodCard from './FoodCard'
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Image from 'next/image.js'
+import exportData from '../exportData.js'
 /* eslint-enable no-unused-vars */
 
 export default async function FoodList() {
 	const data = await fetchDataFoods()
 	return (
-		<div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 overflow-y-scroll relative top-28">
+	<div className="">
+	<div className="h-12 w-12 top-28 absolute" >
+				<button className=" bg-green-400 h-8 w-8 rounded-full shadow-2xl mt-3 mr-2" onClick={()=>exportData(data,'Comidass')}>
+					<Image
+						src="/excel.svg"
+						className="ml-2"
+						width={15}
+						height={15}>	
+					</Image>
+				</button>
+			</div>
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 overflow-y-scroll relative top-28">
 			{data.map(food => (
 					<FoodCard key={food.id} food={food} />
 			))}
 		</div>
+		</div>
+
 	)
 }
