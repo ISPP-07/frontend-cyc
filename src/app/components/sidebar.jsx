a8230bff6e2a172f24f50497ebbac95786303d0b
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-07 14:19:48 +0100

    feat: final version sidebar

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,14 +1,72 @@
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import './components.css'
 import React from 'react'
 import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import SidebarEntry from './SidebarEntry'
 
 export default function Sidebar() {
 	const searchParams = useSearchParams()
 	const pathname = usePathname()
 	const { replace } = useRouter()
+
+	const links = [
+		{
+			link: '/families',
+			icon: '/family.svg',
+			text: 'Familias'
+		},
+		{
+			link: '/families?show=true',
+			icon: '/square-plus.svg',
+			text: 'Dar de alta',
+			subentry: true
+		},
+		{
+			link: '',
+			icon: '/no-family.svg',
+			text: 'Familias de baja',
+			subentry: true
+		},
+		{
+			link: '/food',
+			icon: '/box.svg',
+			text: 'Inventario'
+		},
+		{
+			link: '',
+			icon: '/square-plus.svg',
+			text: 'Añadir elemento',
+			subentry: true
+		},
+		{
+			link: '',
+			icon: '/truck.svg',
+			text: 'Entregas'
+		},
+		{
+			link: '',
+			icon: '/square-plus.svg',
+			text: 'Añadir entregas',
+			subentry: true
+		},
+		{
+			link: '',
+			icon: '/bell.svg',
+			text: 'Notificaciones'
+		},
+		{
+			link: '',
+			icon: '/face.svg',
+			text: 'Usuarios'
+		},
+		{
+			link: '/create-user',
+			icon: '/face-plus.svg',
+			text: 'Crear nuevo usuario',
+			subentry: true
+		}
+	]
 
 	let state = searchParams?.get('showSidebar') === 'true'
 
@@ -20,10 +78,10 @@ export default function Sidebar() {
 
 	return (
 		<div
-			className={`${state ? 'min-w-[300px] w-[300px] max-w-[300px]' : 'min-w-[30px] w-[30px] max-w-[30px] sm:w-screen'} sticky top-0 left-0 border border-solid h-screen shadow-xl z-20 bg-white`}
+			className={`${state ? 'min-w-[300px] w-[300px] max-w-[300px] fixed sm:sticky' : 'max-w-0 min-w-0 w-0 sm:min-w-[30px] sm:w-[30px] sm:max-w-[30px] sticky'} top-0 left-0 border border-solid h-screen shadow-xl z-20 bg-white`}
 		>
 			<button
-				className={`${state ? 'left-[280px]' : 'left-[10px]'} absolute cursor-pointer border rounded-full border-gray-300 w-[40px] h-[40px] top-5 bg-blue-400 flex items-center justify-center`}
+				className={`${state ? 'left-[280px]' : 'left-[10px]'} absolute cursor-pointer border rounded-full border-gray-300 w-[40px] h-[40px] top-5 bg-blue-400 hover:bg-blue-600 flex items-center justify-center`}
 				onClick={toggleShowSidebar}
 			>
 				<svg
@@ -52,68 +110,16 @@ export default function Sidebar() {
 			</div>
 			<div className="flex flex-col justify-between">
 				<div className={`${state ? '' : 'hidden'} flex flex-col my-3`}>
-					<Link href="/families" className="">
-						<div className="w-[30px] h-full"></div>
-						<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 py-2 gap-3">
-							<Image src="/family.svg" width={18} height={18}></Image>
-							<span className="font-Varela text-base">Familias</span>
-						</div>
-					</Link>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 gap-3 py-2 pl-10">
-						<Image src="/square-plus.svg" width={18} height={18}></Image>
-						<Link href="/families?show=true" className="font-Varela text-base">
-							Dar de alta
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 gap-3 py-2 pl-10">
-						<Image src="/no-family.svg" width={18} height={18}></Image>
-						<Link href="" className="font-Varela text-base">
-							Familias de baja
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 py-2 gap-3">
-						<Image src="/box.svg" width={18} height={18}></Image>
-						<Link href="/food" className="font-Varela text-base">
-							Inventario
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 gap-3 py-2 pl-10">
-						<Image src="/square-plus.svg" width={18} height={18}></Image>
-						<Link href="" className="font-Varela text-base">
-							Añadir elemento
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 py-2 gap-3">
-						<Image src="/truck.svg" width={18} height={18}></Image>
-
-						<Link href="" className="font-Varela text-base">
-							Entregas
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 gap-3 py-2 pl-10">
-						<Image src="/square-plus.svg" width={18} height={18}></Image>
-						<Link href="" className="font-Varela text-base">
-							Añadir entregas
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 py-2 gap-3">
-						<Image src="/bell.svg" width={18} height={18}></Image>
-						<Link href="" className="font-Varela text-base">
-							Notificaciones
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 py-2 gap-3">
-						<Image src="/face.svg" width={18} height={18}></Image>
-						<Link href="" className="font-Varela text-base">
-							Usuarios
-						</Link>
-					</div>
-					<div className="flex items-center justify-start text-xl cursor-pointer sidebar-menu-item px-3 gap-3 py-2 pl-10">
-						<Image src="/face-plus.svg" width={18} height={18}></Image>
-						<Link href="/create-user" className="font-Varela text-base">
-							Crear nuevo ususario
-						</Link>
-					</div>
+					{links.map((link, index) => (
+						<SidebarEntry
+							key={index}
+							link={link.link}
+							icon={link.icon}
+							text={link.text}
+							subentry={link.subentry}
+							pathname={pathname}
+						/>
+					))}
 				</div>
 				<div
 					className={`${state ? '' : 'hidden'} absolute bottom-0 w-[300px] left-[30px]`}
@@ -121,7 +127,7 @@ export default function Sidebar() {
 					<hr className="w-4/5"></hr>
 					<Link
 						href="/"
-						className="flex items-center justify-center text-sm font-normal font-Varela text-white rounded-xl bg-red-500 shadow-xl p-2 w-3/4 my-9 gap-2"
+						className="flex items-center justify-center text-sm font-normal font-Varela text-white rounded-xl bg-red-500 hover:bg-red-700 shadow-xl p-2 w-3/4 my-9 gap-2"
 					>
 						<Image src="/logout.svg" width={18} height={18}></Image>
 						<span>Cerrar Sesión</span>
