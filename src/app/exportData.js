f2be1470393637c396c0c1e5a4066b38d6a146c2
Nicolás Herrera Lobo <nicoloboo02@gmail.com>
2024-03-08 10:47:17 +0100

    feat: Add import families #31
    A button has been added to import
    families from an excel file.

--- src/app/exportData.js
+++ src/app/exportData.js
@@ -1,7 +1,11 @@
-import exportFromJSON from "export-from-json";
+import exportFromJSON from 'export-from-json'
 
-function exportData(datos,nombre) {
-    exportFromJSON({data:datos, fileName: nombre, exportType: exportFromJSON.types.xls})
+function exportData(datos, nombre) {
+	exportFromJSON({
+		data: datos,
+		fileName: nombre,
+		exportType: exportFromJSON.types.xls
+	})
 }
 
-export default exportData;
+export default exportData
--- src/app/families/page.jsx
+++ src/app/families/page.jsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import Modal from './modal.jsx'
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useRef, useState } from 'react'
 /* eslint-enable no-unused-vars */
 import Card from '../components/card.jsx'
 import Sidebar from '../components/sidebar.jsx'
@@ -10,14 +10,37 @@ import Searchbar from '../components/searchbar.jsx'
 import { fetchFamilies } from './fetchFamilies.js'
 import exportData from '../exportData.js'
 import Image from 'next/image.js'
+import axios from 'axios'
+
 
 export default async function BeneficiariesList({ searchParams }) {
+	const fileInputRef = useRef(null);
+
+	const handleFileChange = async (event) => {
+		const selectedFile = event.target.files[0]
+		try{
+			const formData = new FormData()
+			formData.append('file',selectedFile)
+			await axios.post('url/de/import',formData,{
+				headers: {
+					'Content-Type': 'multipart/form-data'
+				}
+			});
+			alert('Datos importados correctamente')
+		}catch(error){
+			console.error(error)
+			alert('Error al importar los datos')
+		}
+	}
+	const handleClick = () => {
+		fileInputRef.current.click();
+	  };
+	
 	const data = await fetchFamilies()
 	const show = searchParams?.show === 'true'
 	const showModal = () => {
 		window.location.href = '/families?show=true'
 	}
-	console.log(data[1])
 
 	return (
 		<div className="flex h-full flex-col md:flex-row overflow-x-hidden">
@@ -27,7 +50,7 @@ export default async function BeneficiariesList({ searchParams }) {
 					<Searchbar onClickFunction={showModal} />
 				</div>
 				<main className="h-screen w-screen max-w-[1600px] p-6 md:p-12 flex flex-col">
-					<div className="h-12 w-12 top-28 absolute" >
+					<div className="h-12 w-max top-28 absolute flex flex-row" >
 						<button className=" bg-green-400 h-8 w-8 rounded-full shadow-2xl mt-3 mr-2" onClick={()=>exportData(data,'Familias')}>
 							<Image
 								src="/excel.svg"
@@ -36,6 +59,11 @@ export default async function BeneficiariesList({ searchParams }) {
 								height={15}>	
 							</Image>
 						</button>
+						<label htmlFor="file"className='bg-green-400 w-32 h-6 mt-4 rounded-full font-Varela text-white cursor-pointer text-center text-sm'>
+							Importar  datos
+						</label>
+						<input type="file" id="file" onChange={handleFileChange} style={{display:'none'}} accept='.xls'/>
+						
 					</div>
 					<div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 overflow-y-scroll relative top-28">
 						{data.map(family => (
