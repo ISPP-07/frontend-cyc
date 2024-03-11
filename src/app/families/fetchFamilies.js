1fe9cdb16a5d6de1ba2f3b43df6ebdcc31b5b4fa
Manuelgruiz <manuelgruiz22@gmail.com>
2024-03-11 18:03:19 +0100

    fix:Solve vercel problems #38

--- src/app/families/fetchFamilies.js
+++ src/app/families/fetchFamilies.js
@@ -4,7 +4,7 @@ export async function fetchFamilies() {
     const BASEURL = process.env.BASEURL
     try	{		
         const beneficiaries = await axios.get(
-            `${BASEURL}/cyc/family`)
+            `${BASEURL}/cyc/family/`)
         return beneficiaries.data
     } catch (err) {
         console.log(err)
--- src/app/food/fetch.jsx
+++ src/app/food/fetch.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 export async function fetchDataFoods() {
 	const BASEURL = process.env.BASEURL
 	try {
-		const foods = await axios.get(`${BASEURL}/cyc/product`)
+		const foods = await axios.get(`${BASEURL}/cyc/product/`)
 		return foods.data
 	} catch (err) {
 		console.log(err)
