c739e086801570c6c3d8e59dc554512f05b4f679
Ronald752 <ronmonalb@alum.us.es>
2024-03-25 22:06:18 +0100

    feat: Add form with new parameters

--- src/app/components/DeliveriesForm.jsx
+++ src/app/components/DeliveriesForm.jsx
@@ -1,14 +1,22 @@
 'use client'
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 /* eslint-enable no-unused-vars */
 import axios from 'axios'
+import { fetchFamilies } from '../families/fetchFamilies'
+import Select from 'react-select'
+import { fetchDataFoods } from '../food/fetchDataFoods'
 
 function DeliveriesForm({ onClickFunction }) {
+	const [families, setFamilies] = useState([])
+	const [products, setProducts] = useState([])
+
 	const [formData, setFormData] = useState({
-		family: '',
 		date: '',
-		products: [{ name: '', quantity: '' }]
+		months: '',
+		state: '',
+		lines: [{ product_id: '', quantity: '', state: '' }],
+		family_id: ''
 	})
 
 	const handleInputChange = e => {
@@ -17,43 +25,89 @@ function DeliveriesForm({ onClickFunction }) {
 	}
 
 	const handleProductInputChange = (e, index, field) => {
-		const { value } = e.target
-		setFormData(prevState => {
-			const updatedProducts = [...prevState.products]
-			updatedProducts[index][field] = value
-			return { ...prevState, products: updatedProducts }
-		})
+		console.log('e:', e)
+		const updatedProducts = [...formData.lines]
+		if (e === null) {
+			console.log('No se ha seleccionado el producto Nº' + index + 1)
+		} else if (field === 'product_id') {
+			updatedProducts[index][field] = e.value
+		} else if (field === 'quantity') {
+			updatedProducts[index][field] = e.target.value
+		} else if (field === 'state') {
+			updatedProducts[index][field] = e.target.value
+		}
+		setFormData(prevState => ({
+			...prevState,
+			lines: updatedProducts
+		}))
 	}
 
 	const handleAddProduct = () => {
 		setFormData(prevState => ({
 			...prevState,
-			products: [...prevState.products, { name: '', quantity: '' }]
+			lines: [...prevState.lines, { product_id: '', quantity: '', state: '' }]
 		}))
 	}
 
 	const handleRemoveProduct = index => {
-		const updatedProducts = [...formData.products]
+		const updatedProducts = [...formData.lines]
 		updatedProducts.splice(index, 1)
-		setFormData({ ...formData, products: updatedProducts })
+		setFormData({ ...formData, lines: updatedProducts })
 	}
 
-	const handleAddDelivery = () => {
+	const handleAddDelivery = e => {
 		const finalFormData = {
-			...formData,
-			state: formData.state || 'Entregado Todo'
+			...formData
 		}
-		axios.post(
-			`https://65e22f03a8583365b317ff53.mockapi.io/food/deliveries`,
-			JSON.stringify(finalFormData),
-			{
-				headers: {
-					'Content-Type': 'application/json'
-				}
+		const BASEURL = process.env.NEXT_PUBLIC_BASE_URL
+		axios.post(`${BASEURL}/cyc/delivery`, JSON.stringify(finalFormData), {
+			headers: {
+				'Content-Type': 'application/json'
 			}
-		)
+		})
 		console.log('Datos del formulario:', finalFormData)
 	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await fetchFamilies()
+				setFamilies(
+					data.map(family => ({
+						value: family.id,
+						label: family.name
+					}))
+				)
+			} catch (error) {
+				console.error('Error al cargar los datos:', error)
+				alert(
+					'Se produjo un error al cargar los datos. Por favor, inténtalo de nuevo.'
+				)
+			}
+		}
+		fetchData()
+	}, [])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await fetchDataFoods()
+				setProducts(
+					data.map(product => ({
+						value: product.id,
+						label: product.name,
+						quantity: product.quantity
+					}))
+				)
+			} catch (error) {
+				console.error('Error al cargar los datos:', error)
+				alert(
+					'Se produjo un error al cargar los datos. Por favor, inténtalo de nuevo.'
+				)
+			}
+		}
+		fetchData()
+	}, [])
 
 	return (
 		<div className="fixed bg-gray-600 bg-opacity-50 h-full w-full flex font-Varela items-center justify-center z-50">
@@ -70,15 +124,39 @@ function DeliveriesForm({ onClickFunction }) {
 					<article className="flex flex-col w-full md:w-5/12">
 						<label htmlFor="nombre">Familia:</label>
 
-						<input
-							type="text"
-							id="family"
-							name="family"
-							value={formData.family}
-							onChange={handleInputChange}
-							placeholder="Bernal Herrera"
-							className="flex items-center border-2 rounded-xl border-gray-200 bg-white p-1 pl-2 w-full"
-						/>
+						<div className="relative flex items-center border-2 rounded-xl border-gray-200 bg-white">
+							<Select
+								className="border-0 w-full"
+								styles={{
+									control: provided => ({
+										...provided,
+										border: 'none',
+										borderRadius: '9999px',
+										boxShadow: 'none',
+										width: '100%'
+									}),
+									menu: provided => ({
+										...provided,
+										borderRadius: '0px'
+									})
+								}}
+								classNamePrefix="Selecciona una familia"
+								defaultValue={{ label: 'Selecciona una familia', value: 0 }}
+								isDisabled={false}
+								isLoading={false}
+								isClearable={true}
+								isRtl={false}
+								isSearchable={true}
+								name="family_id"
+								options={families}
+								onChange={opt =>
+									setFormData({
+										...formData,
+										family_id: opt?.value ? opt.value : null
+									})
+								}
+							/>
+						</div>
 					</article>
 					<article className="flex flex-col w-full md:w-5/12">
 						<label htmlFor="cantidad-total">Fecha:</label>
@@ -92,21 +170,71 @@ function DeliveriesForm({ onClickFunction }) {
 							className="flex items-center border-2 rounded-xl border-gray-200 bg-white p-1 pl-2 w-full"
 						/>
 					</article>
-					{formData.products.map((product, index) => (
+					<article className="flex flex-col w-full md:w-5/12">
+						<label htmlFor="cantidad-total">Cantidad de meses:</label>
+
+						<input
+							type="text"
+							id="months"
+							name="months"
+							value={formData.months}
+							onChange={handleInputChange}
+							placeholder="0"
+							className="flex items-center border-2 rounded-xl border-gray-200 bg-white p-1 pl-2 w-full"
+						/>
+					</article>
+					<article className="flex flex-col w-full md:w-5/12">
+						<label htmlFor="cantidad-total">Estado:</label>
+
+						<input
+							type="text"
+							id="state"
+							name="state"
+							value={formData.state}
+							onChange={handleInputChange}
+							placeholder="Avisado"
+							className="flex items-center border-2 rounded-xl border-gray-200 bg-white p-1 pl-2 w-full"
+						/>
+					</article>
+					{formData.lines.map((product, index) => (
 						<React.Fragment key={index}>
 							<article className="flex flex-col w-full md:w-5/12">
 								<label htmlFor={`product-${index}`}>
 									Nombre del producto {index + 1}:
 								</label>
-								<input
-									type="text"
-									id={`product-${index}`}
-									name={`product-${index}`}
-									value={product.name}
-									onChange={e => handleProductInputChange(e, index, 'name')}
-									placeholder="arroz"
-									className="flex items-center border-2 rounded-xl border-gray-200 bg-white p-1 pl-2 w-full"
-								/>
+								<div className="relative flex items-center border-2 rounded-xl border-gray-200 bg-white">
+									<Select
+										className="border-0 w-full"
+										styles={{
+											control: provided => ({
+												...provided,
+												border: 'none',
+												borderRadius: '9999px',
+												boxShadow: 'none',
+												width: '100%'
+											}),
+											menu: provided => ({
+												...provided,
+												borderRadius: '0px'
+											})
+										}}
+										classNamePrefix="Selecciona un producto"
+										defaultValue={{
+											label: 'Selecciona un producto',
+											value: 0
+										}}
+										isDisabled={false}
+										isLoading={false}
+										isClearable={true}
+										isRtl={false}
+										isSearchable={true}
+										name={`product-${index}`}
+										options={products}
+										onChange={e =>
+											handleProductInputChange(e, index, 'product_id')
+										}
+									/>
+								</div>
 							</article>
 							<article className="flex flex-col w-full md:w-5/12">
 								<label htmlFor={`quantity-${index}`}>Cantidad:</label>
@@ -116,7 +244,19 @@ function DeliveriesForm({ onClickFunction }) {
 									name={`quantity-${index}`}
 									value={product.quantity}
 									onChange={e => handleProductInputChange(e, index, 'quantity')}
-									placeholder="2"
+									placeholder="0"
+									className="flex items-center border-2 rounded-xl border-gray-200 bg-white p-1 pl-2 w-full"
+								/>
+							</article>
+							<article className="flex flex-col w-full md:w-5/12">
+								<label htmlFor="cantidad-total">Estado del producto:</label>
+								<input
+									type="text"
+									id="stateProduct"
+									name="stateProduct"
+									value={product.state}
+									onChange={e => handleProductInputChange(e, index, 'state')}
+									placeholder="Nuevo"
 									className="flex items-center border-2 rounded-xl border-gray-200 bg-white p-1 pl-2 w-full"
 								/>
 							</article>
