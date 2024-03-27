d6cd5206026fef5ce7826d1079b606c5ef33c661
Nicolás Herrera Lobo <nicoloboo02@gmail.com>
2024-03-27 13:06:17 +0100

    fix: Solve linters #53

--- src/app/components/searchbar.jsx
+++ src/app/components/searchbar.jsx
@@ -36,7 +36,6 @@ const Searchbar = ({
 				<div className="lg:block hidden">
 					<ButtonText
 						text={text}
-						handleClick={handleClick}
 						px={'md:px-3'}
 						isRounded={true}
 						color={'bg-[#75AF73]'}
