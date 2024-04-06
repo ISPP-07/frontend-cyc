3815c4f2aebd24352883862f1cf5fbcd059bdd38
Manuelgruiz <manuelgruiz22@gmail.com>
2024-04-06 22:28:37 +0200

    feat:Add delete #81

--- src/app/components/FoodDetails.jsx
+++ src/app/components/FoodDetails.jsx
@@ -3,9 +3,24 @@
 import React, { useEffect, useState } from 'react'
 /* eslint-enable no-unused-vars */
 import { fetchWarehouse } from '../food/warehouse/fetchWarehouse.js'
+import ButtonIcon from './buttonIcon'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 export default function FoodDetails({ food }) {
 	const [almacen, setAlmacen] = useState(null)
+	const router = useRouter()
+
+	function deleteFood() {
+		const BASEURL = process.env.NEXT_PUBLIC_BASE_URL
+		try {
+			axios.delete(`${BASEURL}/acat/warehouse/product/${food}`)
+			console.log(food)
+			router.push('/food')
+		} catch (error) {
+			return null
+		}
+	}
 
 	useEffect(() => {
 		fetchWarehouse(food.warehouse_id)
@@ -20,6 +35,14 @@ export default function FoodDetails({ food }) {
 	return (
 		<div className="flex flex-col gap-5 bg-gray-50 rounded-xl p-10 drop-shadow-lg border border-gray-300 w-[45	0px]">
 			<div className="flex gap-3 justify-end items-center w-full">
+				<ButtonIcon
+					iconpath="/trash.svg"
+					iconWidth={20}
+					iconHeight={20}
+					color={'bg-red-500'}
+					handleClick={deleteFood}
+					datatestid="deleteButton"
+				/>
 				<h1 className="text-center font-poppins text-2xl">
 					<strong>Detalles del producto</strong>
 				</h1>
