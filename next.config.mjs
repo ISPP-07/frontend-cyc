5931f7324a295e8281bef372f7bf7062ac073c74
JaviGarc1a <javgaragu1@alum.us.es>
2024-04-07 17:23:22 +0200

    feat: Implement pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import withPWAInit from '@ducanh2912/next-pwa'
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {}
 
-export default nextConfig
+const withPWA = withPWAInit({
+	dest: 'public',
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavCaching: true,
+	reloadOnOnline: true,
+	swcMinify: true,
+	disable: false,
+	workboxOptions: {
+		disableDevLogs: true
+	}
+})
+
+export default withPWA(nextConfig)
