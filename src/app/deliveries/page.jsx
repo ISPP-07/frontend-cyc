db64924d97fbdc69e4a9bfdff0697fcd42000370
Ronald752 <ronmonalb@alum.us.es>
2024-04-18 22:19:19 +0200

    fix: Solve problems in deliveries view

--- src/app/deliveries/page.jsx
+++ src/app/deliveries/page.jsx
@@ -116,13 +116,13 @@ export default function DeliveriesList() {
 			)
 			setFilteredData(filtered)
 		}
+	}
 
 	const handlePageChange = (event, value) => {
 		setPage(value)
 	}
 	const handleSelect = opt => {
 		setPerPage(opt?.value)
-
 	}
 
 	useEffect(() => {
@@ -182,7 +182,7 @@ export default function DeliveriesList() {
 	}
 
 	return (
-		<main className='flex w-full'>
+		<main className="flex w-full">
 			<Suspense fallback={<div></div>}>
 				<Sidebar />
 			</Suspense>
@@ -199,40 +199,40 @@ export default function DeliveriesList() {
 				/>
 				<div className="h-12 w-max flex flex-row">
 					<button
-						className=' bg-green-400 h-8 w-8 rounded-full shadow-2xl mt-3 mr-2'
+						className=" bg-green-400 h-8 w-8 rounded-full shadow-2xl mt-3 mr-2"
 						onClick={() => exportData(data, 'Entregas')}
 					>
 						<Image
-							src='/excel.svg'
-							className='ml-2'
+							src="/excel.svg"
+							className="ml-2"
 							width={15}
 							height={15}
 						></Image>
 					</button>
 					<label
-						htmlFor='file'
-						className='bg-green-400 w-32 h-6 mt-4 rounded-full font-Varela text-white cursor-pointer text-center text-sm'
+						htmlFor="file"
+						className="bg-green-400 w-32 h-6 mt-4 rounded-full font-Varela text-white cursor-pointer text-center text-sm"
 					>
 						Importar datos
 					</label>
 					<input
-						type='file'
-						id='file'
+						type="file"
+						id="file"
 						onChange={handleFileChange}
 						style={{ display: 'none' }}
-						accept='.xls'
+						accept=".xls"
 					/>
 				</div>
-				<div className='container p-10 flex flex-wrap gap-5 justify-center font-Varela items-center overflow-y-auto'>
-					<div className='w-full overflow-x-auto'>
-						<table className='table-auto w-full'>
+				<div className="container p-10 flex flex-wrap gap-5 justify-center font-Varela items-center overflow-y-auto">
+					<div className="w-full overflow-x-auto">
+						<table className="table-auto w-full">
 							<thead>
 								<tr>
-									<th className='px-4 py-2 border-b'></th>
-									<th className='px-4 py-2 border-b text-center'>Familia</th>
-									<th className='px-4 py-2 border-b text-center'>Estado</th>
-									<th className='px-4 py-2 border-b text-center'>Fecha</th>
-									<th className='px-4 py-2 border-b'></th>
+									<th className="px-4 py-2 border-b"></th>
+									<th className="px-4 py-2 border-b text-center">Familia</th>
+									<th className="px-4 py-2 border-b text-center">Estado</th>
+									<th className="px-4 py-2 border-b text-center">Fecha</th>
+									<th className="px-4 py-2 border-b"></th>
 								</tr>
 							</thead>
 							<tbody>
@@ -241,69 +241,69 @@ export default function DeliveriesList() {
 										<React.Fragment key={index}>
 											<tr
 												key={index}
-												className='cursor-pointer'
-												data-testid='delivery-data'
+												className="cursor-pointer"
+												data-testid="delivery-data"
 											>
 												<td
-													className='px-4 py-2 border-b'
+													className="px-4 py-2 border-b"
 													onClick={() => handleShowProducts(index)}
 												>
-													<Image src='/truck.svg' width={20} height={20} />
+													<Image src="/truck.svg" width={20} height={20} />
 												</td>
 												<td
-													className='px-4 py-2 border-b text-center'
+													className="px-4 py-2 border-b text-center"
 													onClick={() => handleShowProducts(index)}
 												>
 													{names[delivery.family_id]}
 												</td>
-												<td className='px-2 py-2 border-b text-center w-16'>
+												<td className="px-2 py-2 border-b text-center w-16">
 													<select
 														className={`rounded-lg border p-2 ${delivery.state === 'delivered' ? 'bg-red-100 text-red-700' : delivery.state === 'notified' ? 'bg-blue-100 text-blue-700' : delivery.state === 'next' ? 'bg-purple-100 text-purple-700' : ''}`}
 														value={delivery.state}
 														onChange={event => handleStatusChange(event, index)}
 													>
 														<option
-															value='delivered'
-															className='rounded-lg bg-red-100 p-2 text-red-700'
+															value="delivered"
+															className="rounded-lg bg-red-100 p-2 text-red-700"
 														>
 															Entregado Todo
 														</option>
 														<option
-															value='notified'
-															className='rounded-lg bg-blue-100 p-2 text-blue-700'
+															value="notified"
+															className="rounded-lg bg-blue-100 p-2 text-blue-700"
 														>
 															Avisado
 														</option>
 														<option
-															value='next'
-															className='rounded-lg bg-purple-100 p-2 text-purple-700'
+															value="next"
+															className="rounded-lg bg-purple-100 p-2 text-purple-700"
 														>
 															Próximo
 														</option>
 													</select>
 												</td>
 												<td
-													className='px-4 py-2 border-b text-center'
+													className="px-4 py-2 border-b text-center"
 													onClick={() => handleShowProducts(index)}
 												>
 													{date(delivery.date)}
 												</td>
 												<td
-													className='px-4 py-2 border-b text-center'
+													className="px-4 py-2 border-b text-center"
 													onClick={() => handleShowProducts(index)}
 												>
-													<button data-testid='show-delivery'>
+													<button data-testid="show-delivery">
 														{index === expandedRow ? (
 															<Image
-																src='/arrow-sm-down.svg'
-																className='ml-2'
+																src="/arrow-sm-down.svg"
+																className="ml-2"
 																width={15}
 																height={15}
 															></Image>
 														) : (
 															<Image
-																src='/left-dropdown.svg'
-																className='ml-2'
+																src="/left-dropdown.svg"
+																className="ml-2"
 																width={15}
 																height={15}
 															></Image>
@@ -312,33 +312,33 @@ export default function DeliveriesList() {
 												</td>
 											</tr>
 											{expandedRow === index && (
-												<tr className='bg-gray-100'>
-													<td className='px-4 py-2 border-b'>
-														<Image src='/box.svg' width={20} height={20} />
+												<tr className="bg-gray-100">
+													<td className="px-4 py-2 border-b">
+														<Image src="/box.svg" width={20} height={20} />
 													</td>
-													<td colSpan='2' className='px-4 py-2 border-b'>
-														<p className='text-red-500 text-lg pl-10 mb-2'>
+													<td colSpan="2" className="px-4 py-2 border-b">
+														<p className="text-red-500 text-lg pl-10 mb-2">
 															TOTAL A ENTREGAR
 														</p>
 														{delivery.lines.map((product, i) => (
-															<p key={i} className='pl-14'>
+															<p key={i} className="pl-14">
 																{product.quantity} {product.name}
 															</p>
 														))}
 													</td>
 													<td
-														colSpan='2'
-														className='px-4 py-2 border-b text-center'
-														data-testid='delete-update-buttons'
+														colSpan="2"
+														className="px-4 py-2 border-b text-center"
+														data-testid="delete-update-buttons"
 													>
 														<ButtonIcon
-															iconpath='/edit.svg'
+															iconpath="/edit.svg"
 															iconHeight={18}
 															iconWidth={18}
 															border={'border border-blue-500 mr-5'}
 														/>
 														<ButtonIcon
-															iconpath='/cross.svg'
+															iconpath="/cross.svg"
 															iconHeight={18}
 															iconWidth={18}
 															handleClick={() =>
@@ -360,9 +360,9 @@ export default function DeliveriesList() {
 						count={totalPages}
 						initialpage={1}
 						onChange={handlePageChange}
-						className='flex flex-wrap justify-center items-center'
+						className="flex flex-wrap justify-center items-center"
 					/>
-					<div className='flex justify-center items-center m-2'>
+					<div className="flex justify-center items-center m-2">
 						<p>Número de elementos:</p>
 						<Select
 							options={selectOpts}
@@ -370,7 +370,7 @@ export default function DeliveriesList() {
 							isSearchable={false}
 							isClearable={false}
 							onChange={handleSelect}
-							className='m-2'
+							className="m-2"
 						/>
 					</div>
 				</div>
