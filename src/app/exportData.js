2e9ca3fe268443f6f6365aaa57bdebf0095247bc
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-05-04 14:01:58 +0200

    feat: Export families in two different formats

--- src/app/exportData.js
+++ src/app/exportData.js
@@ -1,18 +1,30 @@
-export function exportData(datos, nombre, columnas) {
+export function exportData(datos, nombre, columnas, duplicatedColumns = false) {
 	try {
-		const translated = []
-		// Columnas contains translations for the object keys. ej. {name: 'Nombre'}
-		for (let i = 0; i < datos.length; i++) {
-			translated.push({})
-			for (const key in datos[i]) {
-				if (columnas[key]) {
-					translated[i][columnas[key]] = datos[i][key]
+		const xlsx = require('xlsx')
+		let ws
+		if (!duplicatedColumns) {
+			const translated = []
+			// Columnas contains translations for the object keys. ej. {name: 'Nombre'}
+			for (let i = 0; i < datos.length; i++) {
+				translated.push({})
+				for (const key in datos[i]) {
+					if (columnas[key]) {
+						translated[i][columnas[key]] = datos[i][key]
+					}
 				}
 			}
+			ws = xlsx.utils.json_to_sheet(translated)
+		} else {
+			ws = xlsx.utils.json_to_sheet(datos)
+			// Create array of column names
+			const headers = []
+			for (const key in columnas) {
+				headers.push(columnas[key])
+			}
+			xlsx.utils.sheet_add_aoa(ws, [headers], { origin: 'A1' })
 		}
-		const xlsx = require('xlsx')
+
 		// Create file
-		const ws = xlsx.utils.json_to_sheet(translated)
 		const wb = xlsx.utils.book_new()
 		xlsx.utils.book_append_sheet(wb, ws, 'Data')
 		xlsx.writeFile(wb, `${nombre}.xlsx`)
--- src/app/families/page.jsx
+++ src/app/families/page.jsx
@@ -19,6 +19,8 @@ export default function FamiliesList() {
 	const [data, setData] = useState(null)
 	const [filteredData, setFilteredData] = useState(null)
 	const [showModal, setShowModal] = useState(false)
+	const [showExportModal, setShowExportModal] = useState(false)
+	const [exportFormat, setExportFormat] = useState(null)
 	const [page, setPage] = useState(1)
 	const [totalPages, setTotalPages] = useState(0)
 	const [perPage, setPerPage] = useState(20)
@@ -40,18 +42,168 @@ export default function FamiliesList() {
 
 	const handleExport = async () => {
 		try {
-			const familyData = await fetchFamilies()
-			exportData(familyData.elements, 'Familias', {
-				name: 'Nombre',
-				phone: 'Teléfono',
-				address: 'Dirección',
-				referred_organization: 'Hermandad',
-				next_renewal_date: 'Fecha de renovación',
-				derecognition_state: 'Estado',
-				observation: 'Observaciones',
-				number_of_people: 'Número de personas',
-				members: 'Miembros'
-			})
+			const familyData = (await fetchFamilies()).elements
+			if (exportFormat === 'app') {
+				// Handle passports
+				const dniRegExp = /^\d{8}[A-Z]$/
+				const nieRegExp = /^[XYZ]\d{7}[A-Z]$/
+				data.forEach(fam => {
+					fam.members.forEach(member => {
+						if (
+							!dniRegExp.test(member.nid) &&
+							!nieRegExp.test(member.nid) &&
+							member.nid !== null
+						) {
+							// Add P- to the passport number
+							member.nid = `P-${member.nid}`
+						}
+					})
+				})
+
+				const familyData = []
+				const memberData = []
+				// The excel is divided in two parts, both in one sheet. The first part corresponds to the family data, then there is an empty column and the second part corresponds to the members data.
+				data.forEach((family, index) => {
+					familyData.push({
+						index: index + 1,
+						name: family.name,
+						phone: family.phone,
+						address: family.address,
+						next_renewal_date: new Date(
+							family.next_renewal_date
+						).toLocaleDateString('es-ES'),
+						derecognition_state:
+							family.derecognition_state === 'Active' ? 'Activa' : 'Suspendida',
+						referred_organization: family.referred_organization,
+						observation: family.observation
+					})
+					family.members.forEach(member => {
+						memberData.push({
+							index_m: index + 1,
+							// Birth Date in format DD/MM/YYYY
+							date_birth: new Date(member.date_birth).toLocaleDateString(
+								'es-ES'
+							),
+							name_m: member.name,
+							surname: member.surname,
+							nationality: member.nationality,
+							nid: member.nid,
+							family_head: member.family_head,
+							gender: member.gender === 'Man' ? 'Hombre' : 'Mujer',
+							functional_diversity: member.functional_diversity ? 'X' : '',
+							// Food intolerances as a string of values separated by commas
+							food_intolerances: member.food_intolerances.join(', '),
+							homeless: member.homeless ? 'X' : ''
+						})
+					})
+				})
+
+				// Fuse family data and member data into one array with one empty column between them
+				const expData = []
+				for (let i = 0; i < memberData.length; i++) {
+					if (familyData[i]) {
+						const element = {
+							...familyData[i],
+							emptyColumn: '',
+							...memberData[i]
+						}
+						expData.push(element)
+					} else {
+						const element = {
+							index: '',
+							name: '',
+							phone: '',
+							address: '',
+							next_renewal_date: '',
+							derecognition_state: '',
+							referred_organization: '',
+							observation: '',
+							emptyColumn: '',
+							...memberData[i]
+						}
+						expData.push(element)
+					}
+				}
+				console.log(expData)
+
+				// Define excel columns
+				const cols = {
+					index: 'numero familia',
+					name: 'nombre',
+					phone: 'numero telefono',
+					address: 'direccion',
+					next_renewal_date: 'fecha renovacion',
+					derecognition_state: 'estado',
+					referred_organization: 'organizacion referida',
+					observation: 'observaciones',
+					emptyColumn: '',
+					index_m: 'numero familia',
+					date_birth: 'fecha nacimiento',
+					name_m: 'nombre',
+					surname: 'apellido',
+					nationality: 'nacionalidad',
+					nid: 'documento identidad',
+					family_head: 'cabeza de familia',
+					gender: 'genero',
+					functional_diversity: 'diversidad funcional',
+					food_intolerances: 'intolerancia alimenticia',
+					homeless: 'sin hogar'
+				}
+
+				exportData(expData, 'Familias', cols, true)
+			} else {
+				// Loop throught the families with index and value
+				const data = []
+				familyData.forEach((family, index) => {
+					// Only include families that are not derecognised
+					if (family.derecognition_state === 'Active') {
+						// Loop through the members of the family
+						let underageCounter = 0
+						family.members.forEach(member => {
+							let iniciales
+							if (member.type === 'Child') {
+								underageCounter++
+								iniciales = 'MENOR ' + underageCounter
+							} else {
+								iniciales =
+									member.name
+										.split(' ')
+										.map(name => name[0])
+										.join('') +
+									member.surname
+										.split(' ')
+										.map(name => name[0])
+										.join('')
+								iniciales.toUpperCase()
+							}
+							data.push({
+								'Numero Unidad': index + 1,
+								'Cabeza de Familia Marcar X': member.family_head ? 'X' : '',
+								'Iniciales Nombre y Apellido Beneficiario': iniciales,
+								'NIF/NIE/Pasaporte Beneficiario': member.nid,
+								'Nacionalidad Beneficiario': member.nationality.toUpperCase(),
+								// Birth Date in format YYYY-MM-DD
+								'Fecha Nacimiento Beneficiario': member.date_birth
+									.split('/')
+									.reverse()
+									.join('-'),
+								'Sexo Beneficiario':
+									member.gender === 'Man' ? 'MASCULINO' : 'FEMENINO'
+							})
+						})
+					}
+				})
+				exportData(data, 'Familias', {
+					'Numero Unidad': 'Número Unidad',
+					'Cabeza de Familia Marcar X': 'Cabeza de Familia Marcar X',
+					'Iniciales Nombre y Apellido Beneficiario':
+						'Iniciales Nombre y Apellido Beneficiario',
+					'NIF/NIE/Pasaporte Beneficiario': 'NIF/NIE/Pasaporte Beneficiario',
+					'Nacionalidad Beneficiario': 'Nacionalidad Beneficiario',
+					'Fecha Nacimiento Beneficiario': 'Fecha Nacimiento Beneficiario',
+					'Sexo Beneficiario': 'Sexo Beneficiario'
+				})
+			}
 		} catch (error) {
 			console.error('Error al cargar los datos para la exportación:', error)
 			alert(
@@ -59,6 +211,7 @@ export default function FamiliesList() {
 			)
 		}
 	}
+
 	const handleFileChange = async event => {
 		const selectedFile = event.target.files[0]
 		try {
@@ -154,7 +307,7 @@ export default function FamiliesList() {
 				<div className='h-12 w-max flex flex-row'>
 					<button
 						className=' bg-green-400 h-8 w-8 rounded-full shadow-2xl mt-3 mr-2'
-						onClick={() => handleExport()}
+						onClick={() => setShowExportModal(true)}
 					>
 						<Image
 							src='/excel.svg'
@@ -208,6 +361,41 @@ export default function FamiliesList() {
 				</div>
 			</div>
 			{showModal ? <Modal closeModal={toggleModal} /> : null}
+			{showExportModal ? (
+				/* {  Ask if the user wants the export in the apps format or banco de alimentos format } */
+				<div
+					data-testid='modalOption'
+					className='fixed w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50'
+				>
+					<div className='bg-white w-2/3 md:w-1/3 px-5 py-7 rounded-lg shadow-lg'>
+						<h2 className='text-xl font-bold text-center'>
+							¿En qué formato desea exportar los datos?
+						</h2>
+						<div className='flex justify-center mt-4'>
+							<button
+								className='border border-gray-300 text-black px-4 py-2 rounded-lg shadow-lg mr-2'
+								onClick={() => {
+									setExportFormat('app')
+									setShowExportModal(false)
+									handleExport()
+								}}
+							>
+								Aplicación
+							</button>
+							<button
+								className='border border-gray-300 text-black px-4 py-2 rounded-lg shadow-lg'
+								onClick={() => {
+									setExportFormat('banco')
+									setShowExportModal(false)
+									handleExport()
+								}}
+							>
+								Banco de alimentos
+							</button>
+						</div>
+					</div>
+				</div>
+			) : null}
 		</main>
 	)
 }
